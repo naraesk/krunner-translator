5e92754d64271faeff3647e95a69835ab557854f
David Baum <david.baum@uni-leipzig.de>
2020-09-10 21:22:18 +0200

    update license information

--- api/translateShellProcess.h
+++ api/translateShellProcess.h
@@ -1,7 +1,20 @@
-
-//
-// Created by David Baum on 10.09.20.
-//
+/******************************************************************************
+ *  Copyright (C) 2013 – 2020 by David Baum <david.baum@naraesk.eu>           *
+ *                                                                            *
+ *  This library is free software; you can redistribute it and/or modify      *
+ *  it under the terms of the GNU Lesser General Public License as published  *
+ *  by the Free Software Foundation; either version 2 of the License or (at   *
+ *  your option) any later version.                                           *
+ *                                                                            *
+ *  This library is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ *  Library General Public License for more details.                          *
+ *                                                                            *
+ *  You should have received a copy of the GNU Lesser General Public License  *
+ *  along with this library; see the file COPYING.LIB.                        *
+ *  If not, see <http://www.gnu.org/licenses/>.                               *
+ *****************************************************************************/
 
 #ifndef RUNNERTRANSLATOR_TRANSLATESHELLPROCESS_H
 #define RUNNERTRANSLATOR_TRANSLATESHELLPROCESS_H
