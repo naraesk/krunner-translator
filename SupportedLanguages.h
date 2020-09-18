6726aba09e718482cf5989ccd9232c005b3c7364
David Baum <david.baum@uni-leipzig.de>
2020-09-18 23:31:59 +0200

    add license header

--- SupportedLanguages.h
+++ SupportedLanguages.h
@@ -1,8 +1,21 @@
-
-//
-// Created by David Baum on 18.09.20.
-//
-
+/******************************************************************************
+ *  Copyright (C) 2013 – 2020 by David Baum <david.baum@naraesk.eu>           *
+ *                                                                            *
+ *  This library is free software; you can redistribute it and/or modify      *
+ *  it under the terms of the GNU Lesser General Public License as published  *
+ *  by the Free Software Foundation; either version 2 of the License or (at   *
+ *  your option) any later version.                                           *
+ *                                                                            *
+ *  This library is distributed in the hope that it will be useful,           *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ *  Library General Public License for more details.                          *
+ *                                                                            *
+ *  You should have received a copy of the GNU Lesser General Public License  *
+ *  along with this library; see the file COPYING.LIB.                        *
+ *  If not, see <http://www.gnu.org/licenses/>.                               *
+ *****************************************************************************/
+ 
 #ifndef RUNNERTRANSLATOR_SUPPORTEDLANGUAGES_H
 #define RUNNERTRANSLATOR_SUPPORTEDLANGUAGES_H
 
